22ea9c8d32997f0e5e149a152802b909d641d250
BernhardW <bernhard.woltron@gmail.com>
2023-06-23 11:59:40 +0200

    added DTOs for GroceryItems

---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bernhard Woltron on 23.06.23.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO {
+    
+    // this will be sent to the Client
+    public let id: UUID // this is not in the request
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
